a9c729cdf83947d97e434feafa12899f11c2449b
peterhijma <P.S.Hijma@students.uu.nl>
2015-04-30 11:58:58 +0000

    help

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,3 +1,10 @@
 naam = input("Wat is je naam? ")
-greetings=input("Hoeveel groeten wil je? ")
-print(greetings+' x gegroet, '+naam+'!')
+
+while True:
+    x=input("Hoeveel groeten wil je? ")
+    try:
+        x=int(x)
+        break
+    except ValueError:
+        print('Kies integer')
+print(str(x)+' x gegroet, '+naam+'!')
