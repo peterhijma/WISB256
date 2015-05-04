005568cbcad02e8772ddfad05d211b61f4ac3a17
peterhijma <P.S.Hijma@students.uu.nl>
2015-05-04 13:39:41 +0000

    Poging3

--- Tests/test.py
+++ Tests/test.py
@@ -5,19 +5,18 @@ import math
 n=int(sys.argv[1])
 
 T1 = time.perf_counter()
+document=open(sys.argv[2],'w')
 
 nietpriem = set()
 priem = []
 for i in range(2, n):
-    if i not in nietpriem:
-        priem.append(i)
-        document=open(sys.argv[2],'w')
-        document.write("\n".join(map(lambda x: str(x), priem)))
-        document.close()
-        for j in range(i*i, n+1, i):
-            nietpriem.add(j)
-              
-
+    if i in nietpriem:
+        continue
+    for j in range(i*i, n+1, i):
+        nietpriem.add(j)
+    priem.append(i)          
+document.write("\n".join(map(str, priem)))
+document.close()
 T2 = time.perf_counter() 
 
 print(str(len(priem)), 'priemgetallen gevonden in ', T2 - T1, 'seconden')
