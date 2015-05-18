d073837422df6b98c05bd2f46b5ff4139bc25c4b
peterhijma <P.S.Hijma@students.uu.nl>
2015-05-18 13:20:56 +0000

    opgave4.1

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -3,18 +3,18 @@ import sys
 
 def findRoot(f,a,b,epsilon):
     m=(a+b)/2
+    if math.fabs(a-b)<epsilon:
+        return m
     if f(a)*f(b)>0:
         print('Kies een beter interval')
-        return
+        quit()
     if f(a)*f(b)==0:
         if f(a)==0:
             print('f(a) is een nulpunt')
-            return
+            quit()
         else:
             print('f(b) is een nulpunt')
-            return
-    if math.fabs(a-b)<epsilon:
-        return m
+            quit()
     if f(a)>f(b):
         if f(m)>0:
             return findRoot(f,m,b,epsilon)
@@ -25,6 +25,7 @@ def findRoot(f,a,b,epsilon):
             return findRoot(f,a,m,epsilon)
         else:
             return findRoot(f,m,b,epsilon)
+
         
 #y=findRoot(lambda x:x*x*x,-4,1,.1)
 #if y==False:
--- Opdracht4/uitvoerenbisec.py
+++ Opdracht4/uitvoerenbisec.py
@@ -4,5 +4,5 @@ import math
 def f(x):
     return (x*x*x)+1
 
-root = bisection.findRoot(f,0,3,0.01)
+root = bisection.findRoot(f,-3,3,0.01)
 print(root)
