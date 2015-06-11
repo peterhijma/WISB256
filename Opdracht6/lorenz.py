cb9fc71b9e6facf893a0941fb808616ad46614b4
peterhijma <P.S.Hijma@students.uu.nl>
2015-06-11 13:47:47 +0000

    poging2opdracht6

--- Opdracht6/lorenz.py
+++ Opdracht6/lorenz.py
@@ -1,5 +1,5 @@
 import numpy
-from scipy import odeint
+from scipy.integrate import odeint
 import math
 
 class Lorenz(object):
@@ -8,8 +8,6 @@ class Lorenz(object):
         self.sigma=sigma
         self.rho=rho
         self.beta=beta
-        
-
         
     def solve(self,T,dt):
         def ode(self,h,t):
@@ -22,3 +20,13 @@ class Lorenz(object):
             return [xdt,ydt,zdt]
         tijd=numpy.arange(0,T,dt)
         return odeint(ode,self.inistate, tijd)
+
+#sigma = 10
+#rho = 28
+#beta = 8/3
+#L1 = Lorenz([-1,1,0],sigma,rho,beta)
+#u1 = L1.solve(50,.01)
+#L2 = Lorenz([-1.001,1.001,.001],sigma,rho,beta)
+#u2 = L2.solve(50,.01)
+#print(u1[0,0],u2[0,0]) # print values of x at t=0
+#print(u1[-1,0],u2[-1,0]) # print values of x at t=50
